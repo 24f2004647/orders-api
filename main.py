760d5426869f1aac3f6d5f80859dfe129b0e7cee
24f2004647@ds.study.iitm.ac.in <24f2004647@ds.study.iitm.ac.in>
2026-07-08 21:57:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ WINDOW_SECONDS = 10
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
